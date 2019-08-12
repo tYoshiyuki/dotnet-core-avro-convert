525d2f08af57476d62807077af5534fbcfaea305
yTsunomoto <wasiwasimaru@gmail.com>
2019-08-12 22:49:26 +0900

    add 新規作成。

---
+++ DotnetCoreAvroConvert/Models/Blog.cs
@@ -0,0 +1,45 @@
+﻿using Avro;
+using Avro.Specific;
+using Microsoft.Hadoop.Avro;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace DotnetCoreAvroConvert.Models
+{
+    [DataContract]
+    public class Blog: ISpecificRecord
+    {
+        [DataMember]
+        public int BlogId { get; set; }
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public string Author { get; set; }
+
+        public Schema Schema => Schema.Parse(AvroSerializer.Create<Blog>().WriterSchema.ToString());
+
+        public object Get(int fieldPos)
+        {
+            switch (fieldPos)
+            {
+                case 0: return BlogId;
+                case 1: return Name;
+                case 2: return Author;
+                default: throw new AvroRuntimeException("Bad index " + fieldPos + " in Get()");
+            };
+        }
+
+        public void Put(int fieldPos, object fieldValue)
+        {
+            switch (fieldPos)
+            {
+                case 0: BlogId = (int)fieldValue; break;
+                case 1: Name = (string)fieldValue; break;
+                case 2: Author = (string)fieldValue; break;
+                default: throw new AvroRuntimeException("Bad index " + fieldPos + " in Put()");
+            };
+        }
+    }
+}
---
+++ DotnetCoreAvroConvert/Program.cs
@@ -0,0 +1,49 @@
+﻿using Avro;
+using Avro.File;
+using Avro.Specific;
+using DotnetCoreAvroConvert.Models;
+using Microsoft.Hadoop.Avro;
+using System;
+using System.Collections.Generic;
+
+namespace DotnetCoreAvroConvert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var schema = Schema.Parse(AvroSerializer.Create<Blog>().WriterSchema.ToString());
+
+            var inputs = new List<Blog>
+            {
+                new Blog { BlogId = 101, Name = "Tanaka", Author = "One" },
+                new Blog { BlogId = 201, Name = "Sato", Author = "Two" },
+                new Blog { BlogId = 301, Name = "Suzuki", Author = "Three" }
+            };
+
+            var writer = new SpecificDatumWriter<Blog>(schema);
+            using (var fw = DataFileWriter<Blog>.OpenWriter(writer, "./blog.avro"))
+            {
+                foreach(var blog in inputs)
+                {
+                    fw.Append(blog);
+                }
+            }
+
+            var outputs = new List<Blog>();
+            using (var fr = DataFileReader<Blog>.OpenReader("./blog.avro"))
+            {
+                while (fr.HasNext())
+                {
+                    outputs.Add(fr.Next());
+                }
+            }
+
+            foreach(var b in outputs)
+            {
+                Console.WriteLine("----- Avro → POCO 変換後 -----");
+                Console.WriteLine($"{b.BlogId} {b.Name} {b.Author}");
+            }
+        }
+    }
+}
